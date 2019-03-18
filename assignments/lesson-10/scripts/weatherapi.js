0b6181cd6b03e82d9ccfecca5e0fb07cf2f151e5
tjchecketts12 <che15016@byui.edu>
2019-03-18 15:26:29 -0600

    Update

---
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -0,0 +1,9 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=bb4509ecac6b78adefbfa471450e41df";
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+}

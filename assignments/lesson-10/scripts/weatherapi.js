383cfe644181715314c485ab694bcc2e7798306d
tjchecketts12 <che15016@byui.edu>
2019-03-18 15:37:38 -0600

    Update

--- assignments/lesson-10/scripts/weatherapi.js
+++ assignments/lesson-10/scripts/weatherapi.js
@@ -6,4 +6,6 @@ weatherRequest.send();
 weatherRequest.onload =  function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
+
+    document.getElementById('current-temp').innerHTML=weatherData.main.temp;
 }

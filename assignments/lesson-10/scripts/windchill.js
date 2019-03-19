7b8e561746974acf1b4abb3b66d85544c6b80ca7
tjchecketts12 <che15016@byui.edu>
2019-03-18 18:13:47 -0600

    Update

--- assignments/lesson-10/scripts/windchill.js
+++ assignments/lesson-10/scripts/windchill.js
@@ -1,5 +1,5 @@
 function windchill (speed, temp) {
-let result = 35.74 + .6215 * temp - 35.75 * Math.pow(sspeed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+let result = 35.74 + .6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
 
 document.getElementById('windchill').innerHTML = result.toFixed(1);
 }
